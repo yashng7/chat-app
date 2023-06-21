b5857132dafb6717408d345e95274c759961f73c
YashwantGawande <gawandeyashwant7@gmail.com>
2023-06-21 23:46:28 +0530

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import ThemeChanger from "./ThemeChanger";
+import ThemeChanger from "./utils/ThemeChanger";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Navigation from "./utils/Navigation";
 
 type Props = {};
 
@@ -8,6 +9,7 @@ const Navbar = (props: Props) => {
   return (
     <nav className="flex items-center justify-between p-5">
       <span>ChitChat</span>
+      <Navigation />
       <div className="flex items-center gap-5">
         <ThemeChanger />
         <span>name</span>

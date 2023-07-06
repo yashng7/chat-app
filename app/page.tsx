0d5a832b5cc1b8fc3077b3b9dcab51a581cef734
YashwantGawande <gawandeyashwant7@gmail.com>
2023-07-06 23:55:09 +0530

    lading page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Image from "next/image";
 import { ThemeProvider } from "next-themes";
-import ThemeChanger from "@/components/ThemeChanger";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {

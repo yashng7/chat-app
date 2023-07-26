d32cc86145295f39a0a3150c52720a1ec39204fb
YashwantGawande <gawandeyashwant7@gmail.com>
2023-07-27 00:00:49 +0530

    add sign out button

---
+++ components/SignOutButton.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2, LogOut } from "lucide-react";
+
+export default function SignOutButton() {
+  const { toast } = useToast();
+  const [isSigningOut, setIsSigningOut] = useState<boolean>(false);
+
+  return (
+    <Button
+      variant="ghost"
+      onClick={async () => {
+        setIsSigningOut(true);
+        try {
+          await signOut();
+        } catch (error) {
+          toast({
+            title: "Error",
+            description: "There was a problem signing out",
+            // action: (
+            //   <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            // ),
+          });
+        } finally {
+          setIsSigningOut(false);
+        }
+      }}
+    >
+      {isSigningOut ? (
+        <Loader2 className="w-4 h-4 animate-spin" />
+      ) : (
+        <LogOut className="w-4 h-4" />
+      )}
+    </Button>
+  );
+}

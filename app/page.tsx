f548709a8759f2d43101f405f2285427ce20c62f
YashwantGawande <gawandeyashwant7@gmail.com>
2023-07-28 23:52:18 +0530

    cleanup them provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
-import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Navbar />
-    </ThemeProvider>
-  );
+  return <Navbar />;
 }

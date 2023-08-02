9e7e2367bb6d0b6a5ad269c9c61c048d806eca33
YashwantGawande <gawandeyashwant7@gmail.com>
2023-08-02 23:47:58 +0530

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import ThemeChanger from "./utils/ThemeChanger";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import Navigation from "./utils/Navigation";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Profile from "./Profile";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
-type Props = {};
+// type Props = { session: User };
 
-const Navbar = (props: Props) => {
+const Navbar = (/*{ session }: Props*/) => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getSession();
+        setSession(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData()
+  }, []);
+
+  // const session = await getAuthSession()
+
   return (
     <nav className="flex items-center justify-between p-5">
       <span>ChitChat</span>
-      <Navigation />
+      {/* <Navigation /> */}
       <div className="flex items-center gap-5">
         <ThemeChanger />
-        <span>name</span>
-        <Avatar>
-          <AvatarImage src="" alt="avatar" />
-          <AvatarFallback></AvatarFallback>
-        </Avatar>
+        {session?.user ? (
+          <Profile user={session.user} />
+          // <p>{session.user.name}</p>
+        ) : (
+          <Link href={"/login"}>
+            <Button>Create Account</Button>
+          </Link>
+        )}
       </div>
     </nav>
   );

fd6041cfdb9c38679f3c61e807cc35e9d77b491f
YashwantGawande <gawandeyashwant7@gmail.com>
2023-08-02 23:48:44 +0530

    update provider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { FC, ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 interface ProviderProps {
@@ -8,10 +9,10 @@ interface ProviderProps {
 
 const Providers: FC<ProviderProps> = ({ children }) => {
   return (
-    <>
+    <SessionProvider>
       <Toaster reverseOrder={false} />
       {children}
-    </>
+    </SessionProvider>
   );
 };
 

fa3b0f7b0b7f68f423c04db4b50c189a78577989
YashwantGawande <gawandeyashwant7@gmail.com>
2023-08-04 23:48:00 +0530

    add request page

---
+++ app/(dashboard)/dashboard/request/page.tsx
@@ -0,0 +1,41 @@
+import FriendRequests from "@/components/FriendRequests";
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import React from "react";
+
+type Props = {};
+
+const page = async (props: Props) => {
+  const session = await getServerSession(authOptions);
+  if (!session) notFound();
+
+  const incomingSenderIds = (await fetchRedis(
+    "smembers",
+    `user:${session.user.id}:incoming_friend_requests`
+  )) as string[];
+
+  const incomingFriendRequests = await Promise.all(
+    incomingSenderIds.map(async (senderId) => {
+      const sender = (await fetchRedis("get", `user:${senderId}`)) as User;
+      return {
+        senderId,
+        senderEmail: sender.email,
+      };
+    })
+  );
+  return (
+    <main className="pt-8">
+      <h1 className="mb-8 text-5xl font-bold">Add a friend</h1>
+      <div className="flex flex-col gap-4">
+        <FriendRequests
+          incomingFriendRequests={incomingFriendRequests}
+          sessionId={session.user.id}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default page;

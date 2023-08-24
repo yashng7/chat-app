dc06e361bc7e6a1bbf5a0e0209bbb2c42cd0fead
YashwantGawande <gawandeyashwant7@gmail.com>
2023-08-24 23:59:53 +0530

    add route for adding friend

--- app/api/friends/add/route.ts
+++ app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -11,28 +13,28 @@ export async function POST(req: Request) {
 
     const { email: emailToAdd } = addFriendValidator.parse(body.email);
 
-    const idToAdd = (await fetchRedis(
-      "get",
-      `user:email:${emailToAdd}`
-    )) as string;
-
-    if (!idToAdd) {
-      return new Response("This person does not exist.", { status: 400 });
-    }
-
     const session = await getServerSession(authOptions);
 
     if (!session) {
       return new Response("Unauthorized", { status: 401 });
     }
 
-    if (idToAdd === session.user.id) {
+    const userToAddId = (await fetchRedis(
+      "get",
+      `user:email:${emailToAdd}`
+    )) as string;
+
+    if (!userToAddId) {
+      return new Response("This person does not exist.", { status: 400 });
+    }
+
+    if (userToAddId === session.user.id) {
       return new Response("You cannot add Yourself as friend", { status: 400 });
     }
 
     const isAlreadyAdded = (await fetchRedis(
       "sismember",
-      `user:${idToAdd}:incoming_friend_request`,
+      `user:${userToAddId}:incoming_friend_requests`,
       session.user.id
     )) as 0 | 1;
 
@@ -40,17 +42,39 @@ export async function POST(req: Request) {
       return new Response("Already added this user", { status: 400 });
     }
 
+    const isAlreadyReceiveFriendRequest = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:incoming_friend_requests`,
+      userToAddId
+    )) as 0 | 1;
+
+    if (isAlreadyReceiveFriendRequest) {
+      return new Response(
+        "This user already invite you first please check your friend requests",
+        { status: 400 }
+      );
+    }
+
     const isAlreadyFriends = (await fetchRedis(
       "sismember",
-      `user:${idToAdd}:friends`,
-      idToAdd
+      `user:${session.user.id}:friends`,
+      userToAddId
     )) as 0 | 1;
 
     if (isAlreadyFriends) {
       return new Response("Already friends with this user", { status: 400 });
     }
 
-    await db.sadd(`user :${idToAdd}:incoming_friend_requests`, session.user.id);
+    await pusherServer.trigger(
+      toPusherKey(`user:${userToAddId}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
+    await db.sadd(`user :${userToAddId}:incoming_friend_requests`, session.user.id);
 
     return new Response("ok");
   } catch (error) {

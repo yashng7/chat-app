11cbdf528809d95ae5535ab8a0f3c0da04eeec77
YashwantGawande <gawandeyashwant7@gmail.com>
2023-08-24 23:57:24 +0530

    add fragment

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -24,63 +24,65 @@ export function CreateAccount() {
     try {
       await signIn("google");
     } catch (error) {
-      toast.error("Something went wrong");
+      toast.error("Something went wrong with login");
     } finally {
       setIsLoading(false);
     }
   }
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Create an account</CardTitle>
-          <CardDescription>
-            Enter your email below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid grid-cols-2 gap-6">
-            <Button variant="outline">
-              <AiOutlineGithub className="w-4 h-4 mr-2" />
-              Github
-            </Button>
-            <Button variant="outline" onClick={loginWithGoogle}>
-              <AiOutlineGoogle className="w-4 h-4 mr-2" />
-              Google
-            </Button>
-          </div>
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Create an account</CardTitle>
+            <CardDescription>
+              Enter your email below to create your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-6">
+              <Button variant="outline">
+                <AiOutlineGithub className="w-4 h-4 mr-2" />
+                Github
+              </Button>
+              <Button variant="outline" onClick={loginWithGoogle}>
+                <AiOutlineGoogle className="w-4 h-4 mr-2" />
+                Google
+              </Button>
             </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="px-2 bg-background text-muted-foreground">
-                Or continue with
-              </span>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="px-2 bg-background text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Create account</Button>
-        </CardFooter>
-      </Card>
-      {isLoading ? (
-        <Card className="absolute flex items-center justify-center w-full h-screen">
-          <Loader2 className="animate-spin" />
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Create account</Button>
+          </CardFooter>
         </Card>
-      ) : (
-        ""
-      )}
-    </div>
+        {isLoading ? (
+          <Card className="absolute flex items-center justify-center w-full h-screen">
+            <Loader2 className="animate-spin" />
+          </Card>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
   );
 }
 export default CreateAccount;
